8fb6f170d1d88a8d174c51852e4d403d98eeabcc
Matthew Smuts <matthewsmuts36@gmail.com>
2023-07-18 16:56:19 +0200

    Initial Commit

--- config.php
+++ config.php
@@ -1,3 +1,22 @@
 <?php
+    //host
+    define("HOST", "localhost");
+    //DB
+    define("DBNAME", "courseOffer");
+    //user
+    define("USER", "root");
+    //password
+    define("PASSWORD", "");
+
+    //PDO connection
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME, USER, PASSWORD);
+    //PDO error mode set to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//    if ($conn == true) //check if there is a connection to the database
+//    {
+//        echo "Connection Successfully Made To" . " " . '"'. DBNAME . '"'. " " . "DB";
+//    }
 
 ?>
--- index.php
+++ index.php
@@ -1,143 +1,139 @@
+<?php
+require "config.php";
+
+$products = $conn->query("SELECT * FROM products");
+$products->execute();
+
+$allProducts = $products->fetchAll(PDO::FETCH_OBJ); //fetch all products from the database as object
+?>
+
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--icon packs-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
     <title>CourseOffer</title>
-  </head>
-  <body>
+</head>
+<body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container" style="margin-top: auto">
-        <a class="navbar-brand  text-white" href="#">CourseOffer</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand  text-white" href="#">CourseOffer™</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
+
         </div>
     </div>
-    </nav>
+</nav>
 
-    <div class="container">    
-        <div class="row mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card" >
-                    <img height="213px" class="card-img-top" src="images/PHP.jpg">
-                    <div class="card-body" >
-                        <h5 class="d-inline"><b>PHP Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
-                        <p>Basics Of PHP, OOP, Types</p>
-                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
-     
-                    </div>
+<div class="container">
+    <div class="row mt-5">
+        <?php foreach ($allProducts as $product): ?>
+        <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mt-3">
+            <div class="card">
+                <img height="213px" class="card-img-top" src="images/<?php echo $product->image; ?>">
+                <div class="card-body">
+                    <h5 class="d-inline"><b><?php echo $product->title; ?></b></h5>
+                    <h5 class="d-inline">
+                        <div class="text-muted d-inline">($<?php echo $product->price; ?>/item)</div>
+                    </h5>
+                    <p><?php echo $product->description; ?></p>
+                    <a href="#" class="btn btn-primary w-100 rounded my-2">Buy <i class="fas fa-arrow-right"></i>
+                    </a>
                 </div>
             </div>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mb-5">
-                <div class="card">
-                    <a href="http://localhost/bookstore/shopping/single.php"><img height="213px" class="card-img-top" src="images/Angular.png"></a>
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Angular 15 Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
-                        <p>Basics Of Angular 15 </p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card">
-                    <img     height="213px" class="card-img-top" src="images/Ionic.jpg">
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Ionic Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
-                        <p>Basics of ionic</p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
-                    </div>
-                </div>
-            </div>
-      
-         </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
 
-         </div>
-       
-        <footer class="bg-dark text-white text-center text-lg-start" style="margin-top: 40px">
-        <!-- Grid container -->
-        <div class="container p-4">
-            <!--Grid row-->
-            <div class="row">
+
+<footer class="bg-dark text-white text-center text-lg-start" style="margin-top: 40px">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!--Grid row-->
+        <div class="row">
             <!--Grid column-->
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                 <h5 class="text-uppercase">Footer Content</h5>
 
                 <p>
-                    Thank you for choosing my practice PHP page for your Angular, PHP, and Ionic Basics courses! We appreciate your interest in expanding your skills and knowledge in these technologies.
-                    (Please note that this is a practice page, and no real payments will be processed.)
+                    Thank you for choosing CourseOffer™ for your online course Needs! We appreciate
+                    your interest in expanding your skills and knowledge in these technologies.
+                    (Please note that this is a demo project, and no real payments will be processed.)
                 </p>
             </div>
             <!--Grid column-->
 
             <!--Grid column-->
             <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Links To Profiles</h5>
+                <h5 class="text-uppercase">Online Profiles</h5>
+                <div class="row">
 
-                <ul class="list-unstyled mb-0">
-                <li>
-                    <a href="#!" class="text-white">Link 1</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 2</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 3</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 4</a>
-                </li>
-                </ul>
+                <a href="https://github.com/MatthewSmuts" class="text-white">
+                    <i class="bi bi-github"></i>
+                </a>
+                <br>
+                <a href="https://www.linkedin.com/in/matthew-smuts-886086203/" class="text-white">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+                <br>
+                <a href="https://www.instagram.com/smuts_matz/" class="text-white">
+                    <i class="bi bi-instagram"></i>
+                </a>
+                </div>
             </div>
             <!--Grid column-->
 
             <!--Grid column-->
             <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase mb-0">Links</h5>
+                <h5 class="text-uppercase mb-0">Resources Used:</h5>
 
                 <ul class="list-unstyled">
-                <li>
-                    <a href="#!" class="text-white">Link 1</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 2</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 3</a>
-                </li>
-                <li>
-                    <a href="#!" class="text-white">Link 4</a>
-                </li>
+                    <li>
+                        <a href="https://getbootstrap.com/" class="text-white" style="text-decoration: none"><i class="bi bi-bootstrap-fill"></i> Bootstrap 5</a>
+                    </li>
+                    <li>
+                        <a href="https://icons.getbootstrap.com/" class="text-white" style="text-decoration: none"><i class="bi bi-bootstrap-fill"></i> Bootstrap icons</a>
+                    </li>
+                    <li>
+                        <a href="https://mdbootstrap.com/" class="text-white" style="text-decoration: none"><i class="bi bi-pie-chart"></i> Material Design for Bootstrap</a>
+                    </li>
+                    <li>
+                        <a href="https://fontawesome.com/" class="text-white" style="text-decoration: none"><i class="bi bi-file-font-fill"></i> Font Awesome</a>
+                    </li>
+                    <li>
+                        <a href="https://popper.js.org/" class="text-white" style="text-decoration: none"><i class="bi bi-card-heading"></i> PopperJS</a>
+                    </li>
                 </ul>
             </div>
             <!--Grid column-->
-            </div>
-            <!--Grid row-->
         </div>
-        <!-- Grid container -->
+        <!--Grid row-->
+    </div>
+    <!-- Grid container -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2023 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        </div>
-        <!-- Copyright -->
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
-  </body>
- 
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright: CourseOffer™
+        <!--            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>-->
+    </div>
+    <!-- Copyright -->
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity=""
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity=""
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
