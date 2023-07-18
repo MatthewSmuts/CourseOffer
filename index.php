e9f095b353c57999bedc1c8f582c6427498b6f4a
Matthew Smuts <matthewsmuts36@gmail.com>
2023-07-18 14:16:24 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
+    <title>CourseOffer</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
+    <div class="container" style="margin-top: auto">
+        <a class="navbar-brand  text-white" href="#">CourseOffer</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
+        </div>
+    </div>
+    </nav>
+
+    <div class="container">    
+        <div class="row mt-5">
+            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+                <div class="card" >
+                    <img height="213px" class="card-img-top" src="images/PHP.jpg">
+                    <div class="card-body" >
+                        <h5 class="d-inline"><b>PHP Basics</b> </h5>
+                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
+                        <p>Basics Of PHP, OOP, Types</p>
+                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
+     
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mb-5">
+                <div class="card">
+                    <a href="http://localhost/bookstore/shopping/single.php"><img height="213px" class="card-img-top" src="images/Angular.png"></a>
+                    <div class="card-body">
+                    <h5 class="d-inline"><b>Angular 15 Basics</b> </h5>
+                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
+                        <p>Basics Of Angular 15 </p>
+                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+                <div class="card">
+                    <img     height="213px" class="card-img-top" src="images/Ionic.jpg">
+                    <div class="card-body">
+                    <h5 class="d-inline"><b>Ionic Basics</b> </h5>
+                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
+                        <p>Basics of ionic</p>
+                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
+                    </div>
+                </div>
+            </div>
+      
+         </div>
+
+         </div>
+       
+        <footer class="bg-dark text-white text-center text-lg-start" style="margin-top: 40px">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!--Grid row-->
+            <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Footer Content</h5>
+
+                <p>
+                    Thank you for choosing my practice PHP page for your Angular, PHP, and Ionic Basics courses! We appreciate your interest in expanding your skills and knowledge in these technologies.
+                    (Please note that this is a practice page, and no real payments will be processed.)
+                </p>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Links To Profiles</h5>
+
+                <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 4</a>
+                </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-0">Links</h5>
+
+                <ul class="list-unstyled">
+                <li>
+                    <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-white">Link 4</a>
+                </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2023 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
+  </body>
+ 
+</html>
